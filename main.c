7670f1ab5c94633c071cae778f03f05e668a59fb
Patrick Kennedy <packy.kennedy@gmail.com>
2016-10-15 19:23:03 -0400

    Added initial files
    Added fuse configuration and main code files

---
+++ main.c
@@ -0,0 +1,19 @@
+//-----------------------------------------------
+//  main.c
+//  Patrick Kennedy
+//
+//  Main file for AVR program
+//-----------------------------------------------
+
+#include <avr/io.h>
+
+int main(void)
+{
+
+    // Insert code
+
+    while(1);
+
+
+    return 0;
+}
